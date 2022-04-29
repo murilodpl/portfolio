02748e21f3f145cb8d69356c234f44907a5287a0
Murilo <inayievmpl@gmail.com>
2022-04-29 17:55:48 -0300

    First commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import homeSVG from '../assets/img/home.svg';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Murilo</h1>
+            <p>fodase caralho</p>
+            <img src={homeSVG} alt="Profile Details" />
+        </div>
+    )
+}
