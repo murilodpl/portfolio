9b214a599d7f99e92320c406e0ac8cc766cf469e
Murilo <inayievmpl@gmail.com>
2022-06-21 17:02:58 -0300

    fix: update Lightbox.tsx - fix bug image in footer after closing lightbox

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,11 +1,18 @@
+/**
+ * References
+ * - https://www.youtube.com/watch?v=uKVVSwXdLr0&ab_channel=WebDevSimplified
+ * - https://codepen.io/WebDevSimplified/pen/mdbwgxy
+ */
+
 export default function Lightbox() {
+    // Function
     function handleClick(e: any) {
         if (e.target !== e.currentTarget) return
         document.body.classList.remove('ofhidden')
         e.target.classList.remove('active')
+        e.target.innerHTML = ""
     }
-    // https://www.youtube.com/watch?v=uKVVSwXdLr0&ab_channel=WebDevSimplified
-    // https://codepen.io/WebDevSimplified/pen/mdbwgxy
+
     return (
         <div id="lightbox" onClick={handleClick}></div>
     )
